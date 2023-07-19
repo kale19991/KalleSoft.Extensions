90c31af2bd9c5ec939a59aff637246259c8ff9c3
Leonardo Oliveira <eng_leonardokale@outlook.com>
2023-07-19 16:06:06 -0300

    inicio projeto

---
+++ Test/KalleSoft.Extensions.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace KalleSoft.Extensions.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
