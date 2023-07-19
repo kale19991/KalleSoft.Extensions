296626e8ea6e46ae92628a993e060491aaf16bba
Leonardo Oliveira <eng_leonardokale@outlook.com>
2023-07-19 17:17:09 -0300

    descrição do pacote

---
+++ back/KalleSoft.Extensions/Maney/MoneyExtension.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KalleSoft.Extensions.Maney
+{
+    public static class MoneyExtension
+    {
+        public static int ToCents(this decimal amount)
+        {
+            if (amount <= 0)
+                return 0;
+            
+            var text = amount.ToString("N2")
+                .Replace(",", string.Empty)
+                .Replace(".", string.Empty);
+            if (string.IsNullOrWhiteSpace(text))
+                return 0;
+
+            int.TryParse(text, out var result);
+            return result;
+        }
+    }
+}
