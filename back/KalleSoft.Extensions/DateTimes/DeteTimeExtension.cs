3d272eba113e057b09e0837787b9695a994f374b
Leonardo Oliveira <eng_leonardokale@outlook.com>
2023-07-23 11:20:58 -0300

    novos metodos de extensão

---
+++ back/KalleSoft.Extensions/DateTimes/DeteTimeExtension.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+
+namespace KalleSoft.Extensions.DateTimes
+{
+    public static class DeteTimeExtension
+    {
+        #region DataTime
+        public static DateTime AddBusinessDays(DateTime dt, int nDays)
+        {
+            int weeks = nDays / 5;
+            nDays %= 5;
+            while (dt.DayOfWeek == DayOfWeek.Saturday || dt.DayOfWeek == DayOfWeek.Sunday)
+                dt = dt.AddDays(1);
+
+            while (nDays-- > 0)
+            {
+                dt = dt.AddDays(1);
+                if (dt.DayOfWeek == DayOfWeek.Saturday)
+                    dt = dt.AddDays(2);
+            }
+            return dt.AddDays(weeks * 7);
+        }
+
+        public static int GetWeekInYear(this DateTime date)
+        {
+            CultureInfo ciCurr = CultureInfo.CurrentCulture;
+            int weekNum = ciCurr.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, ciCurr.DateTimeFormat.FirstDayOfWeek);
+            return weekNum + 1;
+        }
+        #endregion
+    }
+}
