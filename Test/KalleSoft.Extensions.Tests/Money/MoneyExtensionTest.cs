d59eee9d8ec7d38196d4a398aa132315e1f5a875
Leonardo Oliveira <eng_leonardokale@outlook.com>
2023-07-23 10:09:44 -0300

    test money

---
+++ Test/KalleSoft.Extensions.Tests/Money/MoneyExtensionTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KalleSoft.Extensions.Tests.Money
+{
+    public class MoneyExtensionTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void ShowldConvertDecimalToInt()
+        {
+            Assert.Pass();
+        }
+    }
+}
